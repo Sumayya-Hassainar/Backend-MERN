e6aac7c965bb33727e1b07ad35142c55d117f856
sumayya <sumayyahassainar37@gmail.com>
2025-11-30 11:33:02 +0530

    commit change add client url

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: CLIENT_URL || "http://localhost:5173",
     credentials: true,
   })
 );
