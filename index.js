b285dc12c2ad0f7e023b4a207ab599e18efa794d
sumayya <sumayyahassainar37@gmail.com>
2025-12-07 12:56:47 +0530

    updted

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: [
-      "http://localhost:5174",
+      "http://localhost:5173",
       "https://silly-cascaron-014ccd.netlify.app",
     ],
     credentials: true,
