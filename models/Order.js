dc80127ae13bc33908a3b12bb83773bc9a613b57
sumayya <sumayyahassainar37@gmail.com>
2025-12-16 02:01:03 +0530

    upadte

--- models/Order.js
+++ models/Order.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema(
   {
-    customer: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    customer: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 
     vendor: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // optional now
 
